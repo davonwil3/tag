079470235bf3e8ec9110ee4a759a0c11a9c44698
Davon Wilson <davontech3@gmail.com>
2025-06-19 03:05:37 -0400

    fix session table naming

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -3,6 +3,7 @@ import {
   ApiVersion,
   AppDistribution,
   shopifyApp,
+  LATEST_API_VERSION
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import { prisma } from "./db.server";
@@ -10,7 +11,7 @@ import { prisma } from "./db.server";
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: ApiVersion.January25,
+  apiVersion: LATEST_API_VERSION,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
